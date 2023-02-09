30349238a977d9e751436b67c999356c164b5436
Jonah Saywonson <tug22941@temple.edu>
2023-02-09 15:37:54 -0500

    Augmented the coffee shop package to include a third attribute. Display prints third attribute.

--- src/coffee/MenuItem.java
+++ src/coffee/MenuItem.java
@@ -6,8 +6,25 @@ public class MenuItem {
 
 	private String name;
 	private double price;
+	private int stock;
 	
+	//default constructor
+	public MenuItem() {
+		
+	}
+	//overloaded constructor that allows two parameters
+	public MenuItem(String name, double price) {
+		this.name = name;
+		this.price = price;
+	}
 	
+	//overloaded constructor that allows three parameters
+	public MenuItem(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	//properties - getters and setters
 	public String getName() {
 		return name;
 	}
@@ -20,7 +37,12 @@ public class MenuItem {
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int quantity) {
+		this.stock = quantity;
+	}
 	
 
 }
