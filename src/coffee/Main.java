f5dd85aa2c52d49b821234cd1f12ee4ede29e579
Jonah Saywonson <tug22941@temple.edu>
2023-02-10 15:56:29 -0500

    Incremented on the Coffee Shop code -  10, 2023

--- src/coffee/Main.java
+++ src/coffee/Main.java
@@ -5,8 +5,21 @@ public class Main {
 	public static void main(String[] args) {
 		CoffeeShop cs = new CoffeeShop();
 		cs.initialize();
-		cs.printMenuItems();
 
+		while (true) {
+			int selection = cs.menuPrompt();
 
+			if (selection == CoffeeShop.PRINT_MENU) {
+				cs.printMenuItems();
+			} else if (selection == CoffeeShop.ORDER_ITEM) {
+				cs.orderItem();
+			}else if(selection == CoffeeShop.VIEW_CART) {
+				cs.viewCart();
+			}else if (selection == CoffeeShop.EXIT) {
+				System.out.println("Thank you! Please, come again.");
+				// tell the JVM to exit with a status code
+				System.exit(0);
+			}else {}
+		}
 	}
 }
